8b3e79f54c7e29c036cdd643d5880f1377fec308
Veerakran Sereerungruangkul <thisisvegas007@gmail.com>
2023-04-05 20:20:26 +0700

    Fix `TS1183` error in the declaration file

--- index.d.ts
+++ index.d.ts
@@ -1,25 +1,11 @@
 declare module 'localdata' {
-    import ldb from 'localstoragedb'
-
     class ldbInterface {
-        static get(key: string, callback: (string) => void): void {
-            ldb.get(key, callback);
-        }
-        static set(key: string, val: string, callback?: () => void): void {
-            ldb.set(key, val, callback);
-        }
-        static delete(key: string, callback?: () => void): void {
-            ldb.delete(key, callback);
-        }
-        static list(callback: (result: string[]) => void): void {
-            ldb.list(callback);
-        }
-        static getAll(callback: (result: string[]) => void): void {
-            ldb.getAll(callback);
-        }
-        static clear(callback?: () => void): void {
-            ldb.clear(callback);
-        }
+        static get(key: string, callback: (value: string) => void): void;
+        static set(key: string, val: string, callback?: () => void): void;
+        static delete(key: string, callback?: () => void): void;
+        static list(callback: (result: string[]) => void): void;
+        static getAll(callback: (result: string[]) => void): void;
+        static clear(callback?: () => void): void;
     }
 
     export default ldbInterface;
